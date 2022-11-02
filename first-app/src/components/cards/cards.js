79f6321438b1caa499f09c2d2c3362e8223a71ad
Gustavo Rodrigues <Gustavorr001@gmail.com>
2022-11-02 12:12:52 -0300

    listas e a prop key

--- first-app/src/components/cards/cards.js
+++ first-app/src/components/cards/cards.js
@@ -1,25 +1,20 @@
 import Card from '../card/card'
 
+const cardsTitle = ['titulo card 1', 'titulo card 2', 'titulo card 3']
+
 const Cards = () => {
     return (
         <div>
             <h2>My Cards</h2>
             <div>
-                <Card>
-                    <h3>titulo card 1</h3>
-                    <p>esse é um texto do card 1</p>
-                    <button className='btn'>aceitar</button>
-                </Card>
-                <Card>
-                    <h3>titulo card 2</h3>
-                    <p>esse é um texto do card 2</p>
-                    <button className='btn'>outros</button>
-                </Card>
-                <Card>
-                    <h3>titulo card 3</h3>
-                    <p>esse é um texto do card 3</p>
-                    <button className='btn'>cancelar</button>
-                </Card>
+                {
+                    cardsTitle.map((cardsTitle, index) => (
+                        <Card key={index}>
+                            <h3>{cardsTitle}</h3>
+                            <p>esse é um titulo do card</p>
+                        </Card>
+                    ))
+                }
             </div>
 
         </div>
